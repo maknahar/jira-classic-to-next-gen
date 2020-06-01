b9802c7fb96badac163980da1e910d29aaca7c48
maknahar <maknahar@live.in>
2020-06-01 17:17:26 +0530

    Logging changes

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ Jira does not support CSV import facility in next gen project. This script allow
 ### Preparation
 - Take backup of classic project
 - Generate the Jira API token
+- Make sure your screen elements are matching in old and new project. This setting is available in `project Setting` -> `Issue Types`. e.g If old project has field summary enabled in the screen, enable it for new project as well. To make this easier, just before migration enable all elements for all card type. You can refine them after migration as well in Jira UI. 
 - Create a next gen project in Jira
 - Create all the workflow status lanes in the next jane project and make sure name is matching the statuses exactly. By default, Jira next gen comes with TO DO, In Progress and DONE statuses. Jira does not support creating workflows and new status in workflows programmatically.
 - Jira next gen does not support multiple start and end status. So move all your card to one end status in Jira classic project. Otherwise, You will see unusually high number od cards in the backlog of next gen project.
--- migrate.py
+++ migrate.py
@@ -2,6 +2,7 @@
 # http://docs.python-requests.org
 
 import argparse
+import logging
 from concurrent.futures import ThreadPoolExecutor
 
 import requests
@@ -22,8 +23,11 @@ parser.add_argument("-c", "--clean", type=bool, required=False, default=True,
                     help="Set this flag to try if you want to delete all issues " +
                          "and fix versions in destination project before creating " +
                          "anything new")
+parser.add_argument("-v", '--verbose', type=bool, required=False, default=False,
+                    help="Display debug log")
 
 args = parser.parse_args()
+
 url = args.host
 
 auth = HTTPBasicAuth(args.username, args.password)
@@ -38,6 +42,11 @@ non_migrated_issue = {}
 
 # status -> transition id
 transition_map = {}
+
+if args.verbose:
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+else:
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
 
 def transitions(sample_issue_key):
@@ -69,7 +78,7 @@ def create_fix_versions():
         auth=auth
     )
 
-    print("Found {} released in your source projects. Duplicating them.".format(len(response.json())))
+    logging.info("Found {} released in your source projects. Duplicating them.".format(len(response.json())))
 
     for release in response.json():
         data = {
@@ -89,9 +98,9 @@ def create_fix_versions():
         )
 
         if response.status_code > 300:
-            print("Error in creating release", response.json()["errors"])
+            logging.error("Error in creating release %s", response.json()["errors"])
 
-        print("Created release", data["name"])
+        logging.debug("Created release %s", data["name"])
 
 
 def create_issues(issue_type):
@@ -99,7 +108,7 @@ def create_issues(issue_type):
     max_results_requested = 50
     result_received = max_results_requested
     while max_results_requested == result_received:
-        print("retrieving {} {} from {}".format(max_results_requested, issue_type, start_at))
+        logging.info("Migrating {} {} from {}".format(max_results_requested, issue_type, start_at))
         search_url = url + "/rest/api/2/search?jql=project={} AND issuetype={}&startAt={}&maxResults={}".format(
             args.sourkey,
             issue_type,
@@ -152,197 +161,199 @@ def migrate_comment(issue_key):
             )
 
             if response.status_code > 201:
-                print("Error in adding comment. Please file a bug if you think this is a bug", response.json(),
-                      issue_map[issue_key], comment)
+                logging.error("Error in adding comment. Please file a bug if you think this is a bug %s %s %s",
+                              response.json(),
+                              issue_map[issue_key], comment)
                 non_migrated_issue[issue_key] = response.json()
                 return
-            print("Added comment in ", issue_map[issue_key])
+            logging.debug("Added comment in %s", issue_map[issue_key])
 
 
 def migrate_issue(issue_key):
-    if issue_map.keys():
-        print("Issue {} is already created. Ignoring it", issue_key)
-
-    response = requests.request(
-        "GET",
-        url + "/rest/api/2/issue/" + str(issue_key),
-        headers=headers,
-        auth=auth
-    )
-
-    issue_fields = response.json()['fields']
-    issue_key = response.json()['key']
-
-    data = {
-        'fields': {
-            "summary": issue_fields["summary"],
-            "description": issue_fields["description"],
-            "issuetype": {
-                "name": issue_fields['issuetype']['name']
-            },
-            "project": {
-                "key": args.destkey
-            },
-        }
-    }
-
-    if field_exist(issue_fields, "customfield_10024"):
-        data['fields']["customfield_10016"] = issue_fields["customfield_10024"]
-
-    if field_exist(issue_fields, "priority"):
-        data['fields']["priority"] = {"name": issue_fields["priority"]["name"]}
-
-    fix_versions = list()
-    for version in issue_fields["fixVersions"]:
-        fix_versions.append({"name": version["name"]})
-
-    if len(fix_versions) > 0:
-        data['fields']["fixVersions"] = fix_versions
-
-    if len(issue_fields["labels"]) > 0:
-        data['fields']["labels"] = issue_fields["labels"]
-
-    if field_exist(issue_fields, "assignee") and issue_fields["assignee"]["active"]:
-        data['fields']["assignee"] = {"id": issue_fields["assignee"]["accountId"]}
-
-    if field_exist(issue_fields, "reporter") and issue_fields["reporter"]["active"]:
-        data['fields']["reporter"] = {"id": issue_fields["reporter"]["accountId"]}
-
-    if field_exist(issue_fields, "parent"):
-        data['fields']["parent"] = {"key": issue_map[issue_fields["parent"]["key"]]}
-
-    response = requests.request(
-        "POST",
-        url + "/rest/api/2/issue",
-        headers=headers,
-        auth=auth,
-        json=data
-    )
-
-    if response.status_code > 201:
-        print("Error in creating issue. Please file a bug if you think this is a bug", issue_key, response.json(),
-              data)
-        non_migrated_issue[issue_key] = response.json()
-        return
-
-    key = response.json()["key"]
-    print("Created issue {} for {}".format(key, issue_key))
-
-    issue_map[issue_key] = key
-    transition(key, issue_fields["status"]["name"])
-    migrate_comment(issue_key)
-
-
-def transition(issue_key, status):
-    transitions(issue_key)
-    data = {
-        "transition": {
-            "id": transition_map[status.upper()]
-        }
-    }
-
-    response = requests.request(
-        "POST",
-        url + "/rest/api/2/issue/{}/transitions".format(issue_key),
-        headers=headers,
-        auth=auth,
-        json=data
-    )
-
-    if response.status_code > 201:
-        print("Error in transitioning issue. Please file a bug if you think this is a bug", issue_key, response.json(),
-              data)
-        non_migrated_issue[issue_key] = response.json()
-
-    print("migrated {} to {}", issue_key, status)
-
-
-def delete_release(release):
-    response = requests.request(
-        "DELETE",
-        url + "/rest/api/2/version/" + release["id"],
-        headers=headers,
-        auth=auth
-    )
-
-    if response.status_code >= 400:
-        print("Unable to delete release", release["name"], response.json())
-
-    print("Deleted release", release["name"])
-
-
-def delete_issue(issue):
-    response = requests.request(
-        "DELETE",
-        url + "/rest/api/2/issue/" + issue["key"],
-        headers=headers,
-        auth=auth
-    )
-
-    if response.status_code >= 400:
-        print("Unable to delete ssue", issue["key"], response.json())
-
-    print("Deleted issue", issue["key"])
-
-
-def clean_project():
-    if not args.clean:
-        print("Clean flag is off.")
-        return
-
-    response = requests.request(
-        "GET",
-        url + "/rest/api/2/project/" + args.destkey + "/versions",
-        headers=headers,
-        auth=auth
-    )
-
-    with ThreadPoolExecutor(max_workers=5) as executor:
-        executor.map(delete_release, response.json())
-
-    start_at = 0
-    max_results_requested = 100
-    result_received = 100
-    while max_results_requested == result_received:
-        search_url = url + "/rest/api/2/search?jql=project={}&startAt={}&maxResults={}".format(
-            args.destkey,
-            start_at,
-            max_results_requested)
+    if issue_map.get(issue_key):
+        logging.warning("Issue {} is already migrated to {} . Ignoring it".format(issue_key, issue_map[issue_key]))
 
         response = requests.request(
             "GET",
-            search_url,
+            url + "/rest/api/2/issue/" + str(issue_key),
             headers=headers,
             auth=auth
         )
 
-        data = response.json()
-        result_received = len(data["issues"])
-        start_at = start_at + result_received
+        issue_fields = response.json()['fields']
+        issue_key = response.json()['key']
 
-        with ThreadPoolExecutor() as executor:
-            executor.map(delete_issue, data["issues"])
+        data = {
+            'fields': {
+                "summary": issue_fields["summary"],
+                "description": issue_fields["description"],
+                "issuetype": {
+                    "name": issue_fields['issuetype']['name']
+                },
+                "project": {
+                    "key": args.destkey
+                },
+            }
+        }
 
+        if field_exist(issue_fields, "customfield_10024"):
+            data['fields']["customfield_10016"] = issue_fields["customfield_10024"]
 
-clean_project()
+        if field_exist(issue_fields, "priority"):
+            data['fields']["priority"] = {"name": issue_fields["priority"]["name"]}
 
-create_fix_versions()
+        fix_versions = list()
+        for version in issue_fields["fixVersions"]:
+            fix_versions.append({"name": version["name"]})
 
-for issue_type in ["Epic", "Story", "Task", "Bug", "Sub-task"]:
-    try:
-        create_issues(issue_type)
-    except Exception as e:
-        print('error in creating issue', e)
+        if len(fix_versions) > 0:
+            data['fields']["fixVersions"] = fix_versions
 
-with open('migration.csv', 'w') as f:
-    for key in issue_map.keys():
-        f.write("%s,%s\n" % (key, issue_map[key]))
+        if len(issue_fields["labels"]) > 0:
+            data['fields']["labels"] = issue_fields["labels"]
 
-with open('errors.csv', 'w') as f:
-    for key in non_migrated_issue.keys():
-        f.write("%s,%s\n" % (key, non_migrated_issue[key]))
+        if field_exist(issue_fields, "assignee") and issue_fields["assignee"]["active"]:
+            data['fields']["assignee"] = {"id": issue_fields["assignee"]["accountId"]}
 
-parting_msg = "{} issues are not migrated because of some error. If number of issues are less," \
-              " you can go ahead and migrate them manually from Jira UI. If count is large and error " \
-              "can be solved programmatically, Please raise a bug.\n"
-print(parting_msg.format(len(non_migrated_issue)))
+        if field_exist(issue_fields, "reporter") and issue_fields["reporter"]["active"]:
+            data['fields']["reporter"] = {"id": issue_fields["reporter"]["accountId"]}
+
+        if field_exist(issue_fields, "parent"):
+            data['fields']["parent"] = {"key": issue_map[issue_fields["parent"]["key"]]}
+
+        response = requests.request(
+            "POST",
+            url + "/rest/api/2/issue",
+            headers=headers,
+            auth=auth,
+            json=data
+        )
+
+        if response.status_code > 201:
+            logging.error("Error in creating issue. Please file a bug if you think this is a bug %s %s %s", issue_key,
+                          response.json(),
+                          data)
+            non_migrated_issue[issue_key] = response.json()
+            return
+
+        migrated_key = response.json()["key"]
+        logging.debug("Created issue {} for {}".format(migrated_key, issue_key))
+
+        issue_map[issue_key] = migrated_key
+        transition(migrated_key, issue_fields["status"]["name"])
+        migrate_comment(issue_key)
+
+    def transition(issue_key, status):
+        transitions(issue_key)
+        data = {
+            "transition": {
+                "id": transition_map[status.upper()]
+            }
+        }
+
+        response = requests.request(
+            "POST",
+            url + "/rest/api/2/issue/{}/transitions".format(issue_key),
+            headers=headers,
+            auth=auth,
+            json=data
+        )
+
+        if response.status_code > 201:
+            logging.error("Error in transitioning issue. Please file a bug if you think this is a bug %s %s %s",
+                          issue_key,
+                          response.json(),
+                          data)
+            non_migrated_issue[issue_key] = response.json()
+
+        logging.debug("migrated {} to {}".format(issue_key, status))
+
+    def delete_release(release):
+        response = requests.request(
+            "DELETE",
+            url + "/rest/api/2/version/" + release["id"],
+            headers=headers,
+            auth=auth
+        )
+
+        if response.status_code >= 400:
+            logging.error("Unable to delete release %s %s", release["name"], response.json())
+
+        logging.debug("Deleted release %s", release["name"])
+
+    def delete_issue(issue):
+        response = requests.request(
+            "DELETE",
+            url + "/rest/api/2/issue/" + issue["key"],
+            headers=headers,
+            auth=auth
+        )
+
+        if response.status_code >= 400:
+            logging.error("Unable to delete issue %s %s", issue["key"], response.json())
+
+        logging.debug("Deleted issue %s", issue["key"])
+
+    def clean_project():
+        if not args.clean:
+            logging.warning("Clean flag is off. This can result into duplicate issues in new project")
+            return
+
+        response = requests.request(
+            "GET",
+            url + "/rest/api/2/project/" + args.destkey + "/versions",
+            headers=headers,
+            auth=auth
+        )
+
+        logging.info("Found {} versions in new project. Deleting them".format(len(response.json())))
+
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            executor.map(delete_release, response.json())
+
+        start_at = 0
+        max_results_requested = 100
+        result_received = 100
+        while max_results_requested == result_received:
+            search_url = url + "/rest/api/2/search?jql=project={}&startAt={}&maxResults={}".format(
+                args.destkey,
+                start_at,
+                max_results_requested)
+
+            response = requests.request(
+                "GET",
+                search_url,
+                headers=headers,
+                auth=auth
+            )
+
+            data = response.json()
+            result_received = len(data["issues"])
+            start_at = start_at + result_received
+
+            with ThreadPoolExecutor() as executor:
+                executor.map(delete_issue, data["issues"])
+
+    clean_project()
+
+    create_fix_versions()
+
+    for issue_type in ["Epic", "Story", "Task", "Bug", "Sub-task"]:
+        try:
+            create_issues(issue_type)
+        except Exception as e:
+            logging.error('error in creating issue %s', e)
+
+    with open('migration.csv', 'w') as f:
+        for key in issue_map.keys():
+            f.write("%s,%s\n" % (key, issue_map[key]))
+
+    with open('errors.csv', 'w') as f:
+        for key in non_migrated_issue.keys():
+            f.write("%s,%s\n" % (key, non_migrated_issue[key]))
+
+    parting_msg = "{} issues are not migrated fully or partially because of some error. If number of issues are less," \
+                  " you can go ahead and migrate/update them manually from Jira UI. If count is large and error " \
+                  "can be solved programmatically, Please raise a bug.\n"
+
+    logging.info(parting_msg.format(len(non_migrated_issue)))
